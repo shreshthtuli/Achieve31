85082b36dbd6bbbcd5e5db9ebe7413c148428023
shreshthtuli <shreshthtuli@gmail.com>
2019-09-22 00:11:38 +0530

    Bug fixes in control

--- achieve31.py
+++ achieve31.py
@@ -2,12 +2,12 @@ from predict import *
 from control import *
 
 # val = MonteCarlo(basicPolicy, 700000, True)
-val = TD(basicPolicy, 700000, 0.7, 0.01, 10)
-plot(val)
+# val = TD(basicPolicy, 700000, 0.7, 0.01, 10)
+# plot(val)
 
 
-# q = sarsa(5, 1000000, 0.7, 0.01, 0.1, True)
+q = sarsa(5, 1000000, 0.7, 0.01, 0.1, True)
 # q = Q(5, 1000000, 0.7, 0.01, 0.1)
-# q = tdLambda(100000, 0.7, 0.01, 0.1, 0.5)
-# plotQ(q)
+# q = tdLambda(1000000, 0.7, 0.01, 0.1, 0.5)
+plotQ(q)
 # plotMap(q)
--- control.py
+++ control.py
@@ -22,26 +22,26 @@ def updateQ(q, history, dealer, gamma, alpha, k):
         for j in range(k):
             g += math.pow(gamma, j) * (history[i+j][3] if i+j < len(history) else 0)
         g += math.pow(gamma, k) * (q[history[i+k][2],history[i+k][0],history[i+k][1],dealer-1] if i+k < len(history) else 0)
-        try:
+        if dealer > 0:
             q[history[i][2], history[i][0], history[i][1], dealer-1] += alpha*(g - q[history[i][2], history[i][0], history[i][1], dealer-1])
-        except: pass
     return q
 
 def updateQLambda(q, history, dealer, gamma, alpha, l):
     for i in range(len(history)):
         gLambda = 0; s = 0
         # print("i = ", i, "len = ", len(history))
-        for k in range(1,len(history)-i):
+        for k in range(1,len(history)-i+1):
             g = 0
             for j in range(k):
                 g += math.pow(gamma, j) * (history[i+j][3] if i+j < len(history) else 0)
-            g += math.pow(gamma, k-1) * (q[history[i+k][2],history[i+k][0],history[i+k][1],dealer-1] if i+k < len(history) and history[i+k][0]<4 else 0)
+            g += math.pow(gamma, k-1) * (q[history[i+k][2],history[i+k][0],history[i+k][1],dealer-1] if i+k < len(history) else 0)
             gLambda = (1 - l) * math.pow(l, k-1) * g; # s += (1 - l) * math.pow(l, k-1); print("left",k-1)
-        g = 0; k = len(history)-i-1
+        g = 0; k = len(history)-i
         for j in range(k):
             g += math.pow(gamma, j) * (history[i+j][3] if i+j < len(history) else 0)
-        g += math.pow(gamma, k) * (q[history[i+k][2],history[i+k][0],history[i+k][1],dealer-1] if i+k < len(history) and history[i+k][0]<4 else 0)
-        gLambda = math.pow(l, k) * g; # s += math.pow(l, k); print("done",k)
+        if dealer > 0:
+            g += math.pow(gamma, k) * (q[history[i+k][2],history[i+k][0],history[i+k][1],dealer-1] if i+k < len(history) else 0)
+        gLambda += math.pow(l, k) * g; # s += math.pow(l, k); print("done",k)
         # print(s)
         try:
             q[history[i][2], history[i][0], history[i][1], dealer-1] += alpha*(gLambda - q[history[i][2], history[i][0], history[i][1], dealer-1])
@@ -64,7 +64,6 @@ def sarsa(k, episodes, gamma, alpha, e, adaptive=False):
             history.append((sp, sc, convertAction(a), reward)) # sars'a'
             a = aPrime
             if done: 
-                history.append((sp, sc, convertAction(a), 0))
                 break
         q = updateQ(q, history, dealer, gamma, alpha, k)
     return q
@@ -99,7 +98,6 @@ def tdLambda(episodes, gamma, alpha, e, l):
             history.append((sp, sc, convertAction(a), reward)) # sars'a'
             a = aPrime
             if done: 
-                history.append((4, sc, convertAction(a), 0))
                 break
         q = updateQLambda(q, history, dealer, gamma, alpha, l)
     return q
